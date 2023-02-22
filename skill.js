aa91477c8926356b5afdc3318462fcf0a3669611
datots <dato_ts@hotmail.com>
2023-02-22 23:41:57 +0400

    JavaScript Homework

---
+++ skill.js
@@ -0,0 +1,49 @@
+
+// let a = prompt('Please enter: ');
+// let b = prompt('Please enter: ');
+
+
+// function euqaler(a,b){
+//     if(typeof(a) === typeof(b)){
+//         if(a === b){
+//             console.log('A equals B');
+//         }else if(a !== b){
+//             console.log('A not equal B');
+//         }
+//     }
+    
+// }
+
+// console.log(euqaler(a,b))
+
+
+
+// let farenheit = prompt('Enter temperature in Farenheit: ');
+
+// function convertor(farenheit){
+//      let C = (farenheit - 32) * 5/9;
+//      let roundedNum = C.toFixed(1);
+//      console.log(roundedNum);
+// }
+
+
+// console.log(convertor(farenheit));
+
+// let a = prompt('Enter first Number');
+// let operator = prompt('Enter Operator: ');
+// let b = prompt('Enter second Number');
+
+// function calculator(a,b,operator){
+      
+// if(operator == '+'){
+//     return a + b;
+// }else if(operator == '-'){
+//     return a - b;
+// }else if(operator == '*'){
+//     return a * b;
+// }else if(operator == '/'){
+//     return a / b;
+// } 
+
+// }
+// console.log(calculator(a,b,operator));
